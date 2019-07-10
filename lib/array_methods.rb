b963a9e0ef9f7e5c3c68494332eb663d1716bd2f
lite-post-0822 <amberlowe40@gmail.com>
2019-07-10 08:10:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,10 @@
-def find_element_index (hill,[1,2,3,4,5,4,3,2,1] 
-  array=(hill[1,2,3,4,5,4,3,2,1])
+def ("hill"[],"Valley","zig_zag" = [1,2,3,4,5,4,3,2,1]
+  array.length.{|index|
+  puts array [index]
+  }
+ end
 end
-
+end
 def find_max_value(array)
   # Add your solution here
 end
