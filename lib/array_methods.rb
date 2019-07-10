f79df8dba4f04893bb188f78729df3760356ec4b
lite-post-0822 <amberlowe40@gmail.com>
2019-07-10 08:46:11 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,10 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def array(hill [1,2,3,4,5,4,3,2,1])(Valley [5,4,3,2,1,0,1,2,3,4,5])(zig_zag[500.4,1000,5,250])
+  array.length.{|index|
+  puts array [index]
+  }
+ end
 end
+
 
 def find_max_value(array)
   # Add your solution here
